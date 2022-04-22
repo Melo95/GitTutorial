389ac2680dfef30e06b8ed0364f5380c7f3a55ac
Melo95 <carmelo.grova95@gmail.com>
2022-04-22 15:29:05 +0200

    second commint on branch

--- Plot3D.py
+++ Plot3D.py
@@ -1,15 +1,13 @@
-# import plot libraries libraries
-from mpl_toolkits.mplot3d import Axes3D
+# import plot libraries
 import matplotlib.pyplot as plt
 # import numpy linear algebra library
 import numpy as np
  
-# create figure and set it to 3d projection
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+# create figure
+fig,ax = plt.subplots()
  
 # 1D arrays for x and y coordinates
-n_points = 300
+n_points = 500
 x_min,x_max = -5,5
 y_min,y_max = -5,5
 X = np.linspace(x_min,x_max, n_points)
@@ -20,16 +18,13 @@ X, Y = np.meshgrid(X, Y)
  
 # Z 2D grid for the plot
 R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
+Z = np.cos(0.75*R)
  
 # plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap='magma',linewidth=0, antialiased=True)
- 
-# Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
+surf = ax.imshow(Z, cmap='magma',origin='lower')
  
 # Add a color bar
-fig.colorbar(surf, shrink=0.5, aspect=5)
+fig.colorbar(surf)
  
 # plot
 plt.show()
